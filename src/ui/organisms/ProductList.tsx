1366230d9055bd2843a1f9a216d9181f24d869f1
dark82@op.pl <kualaLumpur59>
2024-02-07 23:01:14 +0100

    Add in product list data-testid product-list

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -7,7 +7,7 @@ export const ProductList = ({
 	products: ProductItemType[];
 }) => {
 	return (
-		<ul className="md-grid-cols-3 my-8 grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
+		<ul data-testid="products-list" className="md-grid-cols-3 my-8 grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
